12d6dbc4fc707e10bd3bfee5a442653499ec9258
Catalin Marius Untea <catalin.untea@gmail.com>
2024-01-02 15:52:14 +0200

    init commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,7 +29,7 @@ const createSendToken = (results, statusCode, req, res) => {
     httpOnly: true,
     /*** ACTIVATE LATER ***/
     secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-    sameSite: none,
+    sameSite: 'none',
   };
 
   res.cookie('jwt', token, cookieOptions);
