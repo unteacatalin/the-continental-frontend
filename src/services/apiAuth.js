6e830a74f6467a4454ecb2041351fd5add03bee8
Catalin Untea <catalin.untea@gmail.com>
2024-02-19 22:49:29 +0200

    changed package.jron

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -50,7 +50,7 @@ export async function login({ email, password }) {
       email,
       password,
     }),
-    // withCredentials: true,
+    withCredentials: true,
   });
   // {
   //   method: 'POST',
