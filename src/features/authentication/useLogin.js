a31cd1f21e9ccef468296c3b08841bfd37107b81
Catalin Untea <catalin.untea@gmail.com>
2024-02-20 11:49:28 +0200

    changed useLogin hook

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,7 +10,7 @@ export function useLogin() {
   const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
-    mutationFn: loginApi,
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       console.log({ user });
       const userData = { ...user, jwt_expiry: Date.now() + jwtExpiry };
