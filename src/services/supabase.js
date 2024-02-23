bad3868952e46d530e9218bacfcfef854ed65fd6
Catalin Untea <catalin.untea@gmail.com>
2024-02-24 01:56:10 +0200

    changed getRooms apiRooms

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -5,12 +5,7 @@ export const supabaseUrl = 'https://mbehgukaiafkgmqfeboa.supabase.co';
 // JWT expiry time
 export const jwtExpiry = 6 * 60 * 60 * 1000;
 
-let supabaseKey;
-if (import.meta.env.NETLIFY === 'true') {
-  supabaseKey = process.env.VITE_SUPABASE_KEY;
-} else {
-  supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-}
+const  supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
