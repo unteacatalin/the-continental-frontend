8cde00e778f60f1941f2c1078753c9ff75a13355
Catalin Untea <catalin.untea@gmail.com>
2024-02-29 16:12:04 +0200

    corrected wrong changes

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -25,7 +25,7 @@ export async function login({ email, password }) {
   //   password,
   // });
   const {
-    data: { user },
+    data,
     error,
   } = await axios({
     method: 'POST',
@@ -41,9 +41,11 @@ export async function login({ email, password }) {
     // withCredentials: true,
   });
 
+  console.log({data});
+
   if (error) throw new Error(error.message);
 
-  return user;
+  return data.user;
 
 //   if (error) throw new Error(error.message);
 
