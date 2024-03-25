4eee53dbcaaacb9e7d83a47555e44bce57161ad0
Catalin Untea <catalin.untea@gmail.com>
2024-03-25 12:47:05 +0200

    implemented create/edit room

--- src/features/rooms/CreateCabinForm.jsx
+++ src/features/rooms/CreateCabinForm.jsx
@@ -77,7 +77,7 @@ function CreateCabinForm({ onCloseModal, roomToEdit = {} }) {
     } else {
       const formData = new FormData();
       formData.append("image", typeof data.image[0] === 'file' ?? data.image[0]);
-      console.log({ image: data.image[0], dataForm });
+      console.log({ image: data.image[0], formData });
 
       createRoom(
         { ...data, image, formData },
