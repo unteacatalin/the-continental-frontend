417a59c7d0644628e3ca984c55a70f6d2697692e
Catalin Untea <catalin.untea@gmail.com>
2024-04-30 17:56:38 +0300

    implemented filter guests

--- src/features/rooms/useRooms.js
+++ src/features/rooms/useRooms.js
@@ -25,7 +25,7 @@ export function useRooms() {
   // QUERY
   const {
     isLoading,
-    data: rooms,
+    data: rooms, count, from, to, PAGE_SIZE,
     error,
   } = useQuery({
     queryKey: ['rooms', filter, sortBy, page],
