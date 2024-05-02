fb375ce3736d750bbc96272f06ce39dfcf4389cd
Catalin Untea <catalin.untea@gmail.com>
2024-05-02 17:52:34 +0300

    implemented filter rooms

--- src/features/rooms/useRooms.js
+++ src/features/rooms/useRooms.js
@@ -24,7 +24,7 @@ export function useRooms() {
 
   // QUERY
   const {
-    data: {rooms, count, from, to, PAGE_SIZE},
+    data: { data: { rooms, count, from, to, PAGE_SIZE }},
     isLoading,
     error,
   } = useQuery({
